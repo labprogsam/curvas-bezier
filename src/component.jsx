b5cd79515d913d8a0af0d9ff81904458ef879a1d
labprogsam <som3@cin.ufpe.br>
2021-04-22 14:27:42 -0300

    Feature(base): Chanve div by canvas

--- src/component.jsx
+++ src/component.jsx
@@ -1,47 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function Board() {
   const [points, setPoints] = useState([]);
-
-  const createLine = (x1, y1, x2, y2) => {
-    let distance = Math.sqrt( ((x1-x2) * (x1-x2)) + ((y1-y2) * (y1 - y2)) );
-
-    let xMid = (x1 + x2)/2;
-    let yMid = (y1 + y2)/2;
-
-    let salopeInRadian = Math.atan2(y1 - y2, x1 - x2);
-    let salopeInDegree = (salopeInRadian  * 180) / Math.PI;
-
-    return ({ width: distance, top: yMid, left: xMid, transform: `rotate(${salopeInDegree}deg)`, backgroundColor: '#000' })
-  }
+  const [showLines, setShowLines] = useState(true);
 
   const newPoint = (event) => {
-    setPoints([...points, { x: event.clientX, y: event.clientY }]);
+    setPoints([...points, { x: event.clientX , y: event.clientY }])
   }
 
-  return (
-    <div className="App" onClick={(event) => newPoint(event)}>
-      {
-        points?.map((item, index) => {
-          return (
-            <>
-              <div
-                key={`${item.x}-${item.y}`}
-                className={`point ${item.x}-${item.y}`}
-                style={{ top: item.y - 8, left: item.x - 8 }}
-              />
-              {index + 1 < points.length &&
-                <div
-                  id={`line-${item.x}-${item.y}-${points[index+1].x}-${points[index+1].y}`}
-                  style={createLine(item.x, item.y, points[index+1].x, points[index+1].y)}
-                />
-              }
-            </>
-          )
-        })
+  useEffect(() => {
+    const canvas = document.querySelector('#screen');
+    const context = canvas.getContext('2d');
+  
+    canvas.width = 1000;
+    canvas.height = 700;
+    canvas.style.margin = 'auto';
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, 1000, 700);
+
+    // desenha os pontos e cria as linhas
+    points?.forEach((item, index) => {
+      context.beginPath();
+      context.arc(item.x, item.y, 4, 0, 2 * Math.PI, true);
+      context.moveTo(item.x, item.y);
+      context.stroke();
+      if (points[index + 1] && showLines) {
+        context.lineTo(points[index + 1].x, points[index + 1].y);
+        context.stroke();
       }
-    </div>
+    });
+
+  }, [points, showLines]);
+
+  return (
+    <canvas id="screen" onClick={(event) => newPoint(event)} />
   );
 }
 
