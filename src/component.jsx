073c10ce36ed2e48b4c2a535f3e2aca5f7255f6b
labprogsam <som3@cin.ufpe.br>
2021-04-22 11:50:15 -0300

    Feature(base): FIrst try

--- src/component.jsx
+++ src/component.jsx
@@ -1,13 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
 
-function Component() {
+function Board() {
+  const [points, setPoints] = useState([]);
+
+  const createLine = (x1, y1, x2, y2) => {
+    let distance = Math.sqrt( ((x1-x2) * (x1-x2)) + ((y1-y2) * (y1 - y2)) );
+
+    let xMid = (x1 + x2)/2;
+    let yMid = (y1 + y2)/2;
+
+    let salopeInRadian = Math.atan2(y1 - y2, x1 - x2);
+    let salopeInDegree = (salopeInRadian  * 180) / Math.PI;
+
+    return ({ width: distance, top: yMid, left: xMid, transform: `rotate(${salopeInDegree}deg)`, backgroundColor: '#000' })
+  }
+
+  const newPoint = (event) => {
+    setPoints([...points, { x: event.clientX, y: event.clientY }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div className="App" onClick={(event) => newPoint(event)}>
+      {
+        points?.map((item, index) => {
+          return (
+            <>
+              <div
+                key={`${item.x}-${item.y}`}
+                className={`point ${item.x}-${item.y}`}
+                style={{ top: item.y - 8, left: item.x - 8 }}
+              />
+              {index + 1 < points.length &&
+                <div
+                  id={`line-${item.x}-${item.y}-${points[index+1].x}-${points[index+1].y}`}
+                  style={createLine(item.x, item.y, points[index+1].x, points[index+1].y)}
+                />
+              }
+            </>
+          )
+        })
+      }
     </div>
   );
 }
 
-export default Component;
+export default Board;
